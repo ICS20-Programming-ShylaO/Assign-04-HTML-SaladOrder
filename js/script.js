cca7314ec513830a89014c95467c8dbba2b69f2b
ShylaOommen <124379324+ShylaOommen@users.noreply.github.com>
2023-05-02 02:13:34 +0000

    ask ms raffin why

--- js/script.js
+++ js/script.js
@@ -22,16 +22,16 @@ function orderSent() {
   let select2 = document.getElementById("drinks");
   let nOfDrinks = select2.options[select2.selectedIndex].value;
   let userDressing = document.getElementById("options-dressing");
-document.getElementById('response').innerHTML = "The salad dressing is " + userDressing
+  document.getElementById('results').innerHTML = userDressing
   //determining the number of checkboxes checked for number of toppings
   let checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
   let nOfToppings = checkboxes.length;
 
   //determining the cost of the toppings using number of toppings x price per topping
   let costToppings = nOfToppings * PRICE_TOPPINGS;
-  // initializing sizeCost as 0 and failed response for when user did not enter a size
+  // initializing sizeCost as 0 and order confirmation for displaying information
   let sizeCost = 0;
-  let failedResponse = "";
+  let orderConfirmation = "";
   //determining the cost of size + drinks using compound if/then statements
   
   //if the size is small
@@ -40,31 +40,37 @@ document.getElementById('response').innerHTML = "The salad dressing is " + userD
   }
   //if the size is medium
   else if (size == "medium") {
-    sizeDrinks = PRICE_MEDIUM;
+    sizeCost = PRICE_MEDIUM;
   }
   //else if the size is large
   else if (size == "large") {
-    sizeDrinks = PRICE_LARGE;
+    sizeCost = PRICE_LARGE;
   }
   //otherwise, the user did not enter a size
   else {
-    failedResponse = "Please enter a size."
+    orderConfirmation = "Please enter a size."
   }
+
   //determining cost of drinks
   drinksCost = nOfDrinks * PRICE_DRINKS;
   //initializing salad dressing for upcoming if statement
   let dressingCost = 0;
-  
+
   //adding 0.50 to the subtotal if the user wants salad dressing
   if (userDressing == "yes") {
     dressingCost = PRICE_DRESSING;
   }
-  
+  else {
+    dressingCost = 0;
+  }
+
   //calculating the subtotal, tax and total
   let subtotal = sizeCost + costToppings + drinksCost + dressingCost;
   let tax = subtotal * HST;
   let total = subtotal + tax;
 
   //displaying the totals to the screen
-  document.getElementById("results").innerHTML = failedResponse + "You ordered: size" + size + ", " + nOfDrinks + " drinks, " + nOfToppings + " toppings, and you said " + userDressing + " to salad dressing. Your subtotal is $" + subtotal.toFixed(2) + ".<br>The amount of HST added is $" + tax.toFixed(2) + ".<br>Your total is $" + total.toFixed(2) + ".";
+  orderConfirmation = "You ordered: size" + size + ", " + nOfDrinks + " drinks, " + nOfToppings + " toppings, and you said " + userDressing + " to salad dressing. Your subtotal is $" + subtotal.toFixed(2) + ".<br>The amount of HST added is $" + tax.toFixed(2) + ".<br>Your total is $" + total.toFixed(2) + ".";
+  
+  document.getElementById("results").innerHTML = orderConfirmation
 }
